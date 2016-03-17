85d311b412dfa3dc386cc263e601b10fb279be05
tianzm <tianzm@asto-inc.com>
2016-03-17 19:14:14 +0800

    网络流量监控脚本

---
+++ python_leaning/remotepython/interface/update.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# coding: utf-8
+import rrdtool
+import time,psutil
+
+total_input_traffic = psutil.net_io_counters()[1]
+total_output_traffic = psutil.net_io_counters()[0]
+starttime = int(time.time())
+update = rrdtool.updatev('/data/learning/python_leaning/remotepython/Flow.rrd','%s:%s:%s' %(str(starttime),str(total_input_traffic),str(total_output_traffic)))
+print update
